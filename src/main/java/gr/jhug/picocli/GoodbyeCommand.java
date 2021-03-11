668fccd8400594825d76bfd52fc99969583ba7b5
Georgios Andrianakis <geoand@gmail.com>
2021-03-11 09:13:40 +0200

    Add first version

---
+++ src/main/java/gr/jhug/picocli/GoodbyeCommand.java
@@ -0,0 +1,21 @@
+package gr.jhug.picocli;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "goodbye")
+public class GoodbyeCommand implements Runnable {
+
+    @Option(names = {"--name"}, description = "Guest name", defaultValue = "world")
+    String name;
+
+    @Option(names = {"--times", "-t"}, defaultValue = "1", description = "How many time should we say goodbye")
+    int times;
+
+    @Override
+    public void run() {
+        for (int i = 0;i<times;i++) {
+            System.out.printf("Goodbye %s!\n", name);
+        }
+    }
+}
